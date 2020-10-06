ee07f277781265b287daaf6cdfd025f2c32a6048
Ernst Eicher <ernst.eicher@outlook.at>
2020-10-06 23:28:32 +0200

    Testeintrag in startup.cs

--- OtripleS.Web.Api/Startup.cs
+++ OtripleS.Web.Api/Startup.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Coalition of the Good-Hearted Engineers
 // FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
 // ---------------------------------------------------------------
+// Nur ein Versuch
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
